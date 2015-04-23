e46bab2039847f876ad08e24fc4796fb2ce5d70c
Chris <s86610000@gmail.com>
2015-04-23 12:22:45 +0800

    Commit 刪除資料功能不能真的刪除資料庫中的資料，並修正 ViewOfReport TSQL語法

--- MVC5HomeWork/Controllers/客戶銀行資訊Controller.cs
+++ MVC5HomeWork/Controllers/客戶銀行資訊Controller.cs
@@ -17,7 +17,7 @@ namespace MVC5HomeWork.Controllers
         // GET: 客戶銀行資訊
         public ActionResult Index()
         {
-            var 客戶銀行資訊 = db.客戶銀行資訊.Include(客 => 客.客戶資料);
+            var 客戶銀行資訊 = db.客戶銀行資訊.Where(p => p.是否已刪除 == false).Include(客 => 客.客戶資料);
             return View(客戶銀行資訊.ToList());
         }
 
@@ -115,7 +115,7 @@ namespace MVC5HomeWork.Controllers
         public ActionResult DeleteConfirmed(int id)
         {
             客戶銀行資訊 客戶銀行資訊 = db.客戶銀行資訊.Find(id);
-            db.客戶銀行資訊.Remove(客戶銀行資訊);
+            客戶銀行資訊.是否已刪除 = true;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
